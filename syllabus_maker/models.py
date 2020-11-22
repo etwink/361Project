44856391c7f15b0292e06266276fb59f70f672ae
Ryan <rjlaflin@uwm.edu>
2020-11-21 19:25:06 -0600

    made changes to models - added fields and changed 'User' to 'MyUser'

--- syllabus_maker/models.py
+++ syllabus_maker/models.py
@@ -10,10 +10,14 @@ class Login(models.Model):
 
 
 # For Users
-class User(models.Model): 
+class MyUser(models.Model):
     name = models.CharField(max_length=20)
     login = models.ForeignKeyLogin(Login, on_delete=models.CASCADE)
-    access = models.IntegerField(max_length=1)
+    access = models.CharField(max_length=1)
+    office = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    office_hours = models.DateTimeField('date published')
 
     def __str__(self):
         return self.name
@@ -21,8 +25,11 @@ class User(models.Model):
 
 class Course(models.Model):
     name = models.CharField(max_length=20)
+    number = models.IntegerField(max_length=20)
+    # For holding course info
+    info = models.CharField(max_length=300)
     # Each course has one instructor, but instructors can have multiple courses.
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(MyUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -33,7 +40,7 @@ class Section(models.Model):
     # Each section has one course, but courses can have multiple sections.
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     # Each section has one TA, but TA's can have multiple sections.
-    teachingAssistant = models.ForeignKey(User, on_delete=models.CASCADE)
+    teachingAssistant = models.ForeignKey(MyUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
