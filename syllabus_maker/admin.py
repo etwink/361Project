48864d0dee7689c0887cb040ceea37dd5cfdfb69
Ryan <rjlaflin@uwm.edu>
2020-11-24 03:10:05 -0600

    added to views - working on login basics

--- syllabus_maker/admin.py
+++ syllabus_maker/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import MyUser, Course, Section, Login
+
+# Register your models here.
+admin.site.register(Login)
+admin.site.register(MyUser)
+admin.site.register(Course)
+admin.site.register(Section)
--- syllabus_maker/models.py
+++ syllabus_maker/models.py
@@ -13,11 +13,12 @@ class Login(models.Model):
 class MyUser(models.Model):
     name = models.CharField(max_length=20)
     login = models.ForeignKeyLogin(Login, on_delete=models.CASCADE)
+    # access level
     access = models.CharField(max_length=1)
     office = models.CharField(max_length=20)
-    phone = models.CharField(max_length=20)
+    phoneNum = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
-    office_hours = models.DateTimeField('date published')
+    officeHours = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
@@ -36,7 +37,7 @@ class Course(models.Model):
 
 
 class Section(models.Model):
-    name = models.CharField(max_length=20)
+    number = models.IntegerField(max_length=3, unique=True, min_value=0, max_value=999)
     # Each section has one course, but courses can have multiple sections.
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     # Each section has one TA, but TA's can have multiple sections.
