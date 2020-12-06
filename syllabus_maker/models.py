de1cae411008993a9a4ecf2fcb0654b1552bcc0d
mcfoster9077 <mfosterc123@gmail.com>
2020-12-06 15:24:39 -0600

    Modified models.py Course method

--- syllabus_maker/models.py
+++ syllabus_maker/models.py
@@ -7,10 +7,10 @@ class MyUser(models.Model):
     password = models.CharField(max_length=20)
     # access level
     access = models.CharField(max_length=1)
-    office = models.CharField(max_length=20)
-    phoneNum = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
-    officeHours = models.CharField(max_length=20)
+    office = models.CharField(max_length=20, blank=True, null=True)
+    phoneNum = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=30, blank=True, null=True)
+    officeHours = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +21,7 @@ class Section(models.Model):
     # Each section has one course, but courses can have multiple sections.
     # course = models.ForeignKey(Course, on_delete=models.CASCADE)
     # Each section has one TA, but TA's can have multiple sections.
-    instructor = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(MyUser, on_delete=models.CASCADE, blank=True, null=True)
 
 
     def __str__(self):
@@ -44,7 +44,7 @@ class Course(models.Model):
     info = models.CharField(max_length=300)
     # Each course has one instructor, but instructors can have multiple courses.
     # instructor = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
