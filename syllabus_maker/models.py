4b573b081033e2e70d079a161912c62239388326
Elliott <etwink@uwm.edu>
2020-12-13 13:01:57 -0600

    updated branch

--- syllabus_maker/models.py
+++ syllabus_maker/models.py
@@ -40,48 +40,6 @@ class Section(models.Model):
     # Rename teachingAssistant to Instructor as sections are both for labs and lectures
     #   (e.g. 337-401:Lecture 333-801:Lab)
     # Each section has one
-    syllabus = models.ForeignKey("Syllabus", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.course.name
-
-class Syllabus(models.Model):
-    # Each syllabus has one course, but courses can have multiple syllabi.
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    year = models.IntegerField()
-    semester = models.CharField(max_length=10)
-
-#probably redundant, might just want to apply Grade directly to Syllabus
-#A set of (integer between 0 and 100, letter grade) pairs
-class GradingScale(models.Model):
-    Syllabus = models.OneToOneField(Syllabus, on_delete=models.CASCADE, blank=True, null=True)
-
-class Grade(models.Model):
-    # Each grade has one grading scale, but grading scales can have multiple grades.
-    gradingScale = models.ForeignKey(GradingScale, on_delete=models.CASCADE)
-    #letter grade
-    letter = models.CharField(max_length=1)
-    #upper bound of letter grade with 2 decimal places (i.e. 89.99 or 100.00)
-    upperBound = models.DecimalField(unique=True, max_digits=5, decimal_places=2)
-    #lower bound of letter grade with 2 decimal places (i.e. 89.99 or 100.00)
-    lowerBound = models.IntegerField(unique=True, max_digits=5, decimal_places=2)
-
-#A set of (integer between 0 and 100, description) pairs.
-class WeightedAssessment(models.Model):
-    # Each weighted assessment has one syllabus, but syllabi can have multiple weighted assessments.
-    syllabus = models.ForeignKey(Syllabus, on_delete=models.CASCADE)
-    #The integers are the weights each grading category, and must add up to 100
-    weight = models.IntegerField()
-    #Description may include html markup which should be rendered as normal.
-    description = models.CharField(max_length=20)
-
-#Each calendar entry is of (date, topic, activity)
-class CalendarEntry(models.Model):
-    # Each calendar entry has one syllabus, but syllabi can have multiple calendar entries.
-    syllabus = models.ForeignKey(Syllabus, on_delete=models.CASCADE, blank=True, null=True)
-    #The dates in a calendar can be updated by changing the start date and keeping all offsets between dates, or by editing dates of individual entries.
-    calendarDate = models.DateField()
-    calendarTopic = models.CharField(max_length=20)
-    calendarActivity = models.CharField(max_length=300)
-
-
